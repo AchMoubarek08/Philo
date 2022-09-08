070aa0a2929be094ee787fafff6219aeef80c304
Achraf Moubarek <amoubare@e1r11p2.1337.ma>
2022-09-08 03:52:39 +0100

    forks

--- philo.c
+++ philo.c
@@ -1,18 +1,12 @@
 #include "philo.h"
 
-pthread_mutex_t mutex;
-
-void    *func()
+void    *func(void *var)
 {
+    t_philo *data = (t_philo *)var;
     pthread_mutex_lock(&mutex);
-    printf("ok\n");
-    printf("okok\n");
-    printf("njdk\n");
-    sleep(2);
-    printf("4\n");
-    printf("5\n");
-    printf("6\n");
+    printf("%d\n", data->id);
     pthread_mutex_unlock(&mutex);
+    return(NULL);
 }
 
 void    initialize(int argc, char **argv, t_init *init)
@@ -23,21 +17,52 @@ void    initialize(int argc, char **argv, t_init *init)
     init->sleep = ft_atoi(argv[4]);
     if(argc == 6)
         init->numb_dinners = ft_atoi(argv[5]);
+    
+}
+
+float get_time_now(void)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+	return(tv.tv_usec * 0.001);
+}
+
+int init_philos(t_philo *philo, t_init *init)
+{
+    int i = 0;
+    philo = malloc(sizeof(t_philo) * init->num_of_philos);
+    pthread_mutex_t *forks = malloc(sizeof(pthread_mutex_t) * init->num_of_philos);
+    while(i < init->num_of_philos)
+    {
+        pthread_mutex_init()
+        philo.left = pthread_mutex_init(&fork[i], NULL);
+        philo,right = pthread_mutex_init(&fork[i + 1), NULL);
+        philo[i].id = i + 1;
+    }
+}
+
+int go_threads(t_philo *philo, t_init *init)
+{
+    int i = 0;
+    while(i < init->num_of_philos)
+    {
+        pthread_create(&philo[i].p, NULL, func, (void *)&philo[i]);
+        pthread_join(philo[i].p, NULL);
+        i++;
+    }
+    return(0);
 }
 
 int main(int argc, char **argv)
 {
     t_init init;
+    t_philo *philo;
     if(argc == 5 || argc == 6)
         initialize(argc, argv, &init);
     else
         ft_error("Wrong number of arguments\n");
-    // pthread_t a, b;
-    // pthread_mutex_init(&mutex, NULL);
-    // pthread_create(&a, NULL, &func, NULL);
-    // pthread_create(&b, NULL, &func, NULL);
-    // pthread_join(a, NULL);
-    // pthread_join(b, NULL);
-    // pthread_mutex_destroy(&mutex);
+    init_philos(philo, &init);
+    // go_threads(philo, &init);
     return(0);
 }
--- philo.h
+++ philo.h
@@ -9,6 +9,7 @@
 
 typedef struct s_init
 {
+    int dead_flag;
     int num_of_philos;
     int die;
     int eat;
@@ -16,10 +17,18 @@ typedef struct s_init
     int numb_dinners;
 }t_init;
 
+typedef struct s_philo
+{
+    pthread_t p;
+    pthread_mutex_t *right;
+    pthread_mutex_t *left;
+    t_init *init;
+    int id;
+}t_philo;
+
 void	ft_error(char *str);
 void	ft_putstr_fd(char *s, int fd);
 int     ft_atoi(const char *str);
 int     ft_isdigit(int c);
-
 
 #endif
